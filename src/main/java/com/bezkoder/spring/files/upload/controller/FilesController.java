15dd8b3341a104c5c3f06fa353a99a33a495e5b6
Alok <allstaralok@gmail.com>
2022-03-22 01:04:30 +0530

    Multi file support

--- src/main/java/com/bezkoder/spring/files/upload/controller/FilesController.java
+++ src/main/java/com/bezkoder/spring/files/upload/controller/FilesController.java
@@ -1,25 +1,22 @@
 package com.bezkoder.spring.files.upload.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.bezkoder.spring.files.upload.message.ResponseMessage;
+import com.bezkoder.spring.files.upload.model.FileInfo;
+import com.bezkoder.spring.files.upload.service.FilesStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-import com.bezkoder.spring.files.upload.message.ResponseMessage;
-import com.bezkoder.spring.files.upload.model.FileInfo;
-import com.bezkoder.spring.files.upload.service.FilesStorageService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @CrossOrigin("http://localhost:8081")
@@ -29,18 +26,23 @@ public class FilesController {
   FilesStorageService storageService;
 
   @PostMapping("/upload")
-  public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+  public ResponseEntity<ResponseMessage> uploadFiles(@RequestParam("files") MultipartFile[] files) {
     String message = "";
     try {
-      storageService.save(file);
+      List<String> fileNames = new ArrayList<>();
+      Arrays.asList(files).stream().forEach(file -> {
+        storageService.save(file);
+        fileNames.add(file.getOriginalFilename());
+      });
+      message = ""+fileNames;
 
-      message = "Uploaded the file successfully: " + file.getOriginalFilename();
       return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     } catch (Exception e) {
-      message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+      message = "Fail to upload files!";
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
     }
   }
+
 
   @GetMapping("/files")
   public ResponseEntity<List<FileInfo>> getListFiles() {
